bd17b1e58d2e10893fdfccfbc3da0cd48b74167e
hayato-kame-new <guishan.meisha0330@gmail.com>
2022-03-31 16:00:59 +0900

    Initial commit

---
+++ app/src/main/java/to/msn/wings/kotlincalendarrecyclerview/CurrentMonthFragment.kt
@@ -0,0 +1,22 @@
+package to.msn.wings.kotlincalendarrecyclerview
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CurrentMonthFragment : Fragment() {
+
+    
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_current_month, container, false)
+    }
+
+
+}
